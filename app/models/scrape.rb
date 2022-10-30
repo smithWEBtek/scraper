18361f3dfd2392059f138375c2a3aeef0d23203c
brad@smithwebtek.com <brad@smithwebtek.com>
2022-10-30 19:58:55 -0400

    Try again

--- app/models/scrape.rb
+++ app/models/scrape.rb
@@ -4,18 +4,20 @@ class Scrape < ApplicationRecord
     url = 'https://ultimateclassicrock.com/best-yacht-rock-songs/'
     page = HTTParty.get(url)
     doc = Nokogiri::HTML(page.body)
+# .html5-video-container
 
     # doc.css('.single-post-oembed-youtube-wrapper').each do |node|
-    # doc.css('div').each do |node|
+    doc.css('div').each do |node|
     # doc.css('main div div div').each do |node|
-    doc.css('main div.pod-content').each do |node|
+    # doc.css('main div.pod-content').each do |node|
+    # ".video-stream html5-main-video"
+    # doc.css('div.video-stream').each do |node|
     # check response.body.nil? || response.body.empty?
     binding.pry
 
     end
   end
 end
-
 
 
 
